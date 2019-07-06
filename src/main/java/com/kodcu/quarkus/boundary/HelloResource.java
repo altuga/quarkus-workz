d8a80dd63406fd9381aca1e5234aa29c1a3deda8
AltugAltintas <altuga@gmail.com>
2019-07-07 00:40:49 +0300

    lean mode on

--- src/main/java/com/kodcu/quarkus/boundary/HelloResource.java
+++ src/main/java/com/kodcu/quarkus/boundary/HelloResource.java
@@ -1,5 +1,6 @@
-package com.kodcu.quarkus;
+package com.kodcu.quarkus.boundary;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -8,9 +9,14 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class HelloResource {
 
+    
+    @Inject 
+    HelloService helloService;
+    
+    
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello";
+        return this.helloService.sayHello();
     }
 }
--- src/test/java/com/kodcu/quarkus/NativeHelloResourceIT.java
+++
@@ -1,9 +0,0 @@
-package com.kodcu.quarkus;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeHelloResourceIT extends HelloResourceTest {
-
-    // Execute the same tests but in native mode.
-}
